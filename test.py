c6f20f058ed675c7be4b2eaa674803527f6cdadb
delaguardianick <delaguardianick@gmail.com>
2020-10-05 16:29:48 +0200

    prints doc freq. term freq and pos for every docID

--- test.py
+++ test.py
@@ -1,40 +1,43 @@
 import invert
 
-mainDict, postings = invert.invert('..\cacm.tar\cacm.all', False, False)
+filepath = '..\cacm.tar\cacm.all'
+mainDict, postings = invert.invert(filepath, False, False)
+f = open(filepath, 'r')
+cacm = f.read()
 
-
-def test(pathDict, pathPostings):
+def test():
     term = ""
     # _dict = open(pathDict, "r")
     # postings = open(pathPostings, "r")
     # _dict = _dict.read()
    
     while(term != "ZZEND"):
+        # Ask for user to input the term
         term = input("Term: ")
-        # Doc Frequency
+        # Print doc frequency
         print("Doc Frequency: {}".format(mainDict[term]))
         # 2
         posting = postings[term][0]
-        print(postings[term][0][714])
-        print (posting)
+        # print (posting)
+        # Get all the doc ID's
         keys = posting.keys()
+        # For every doc ID
+        # Print ID, term frequency and positions
         for key in keys:
-            print("Doc ID: {}".format(key))
+            print("\nDoc ID: {}".format(key))
             freq_pos = postings[term][0][key] 
             print("Term Frequency: {}".format(freq_pos[0]))
-            print("Position: {}".format(freq_pos[1]))
-
-
-
-
-
-    
-
+            for i in range(len(freq_pos)):
+                if i != 0:
+                    print("Position: {}".format(freq_pos[1]))
+                
+            
+               
 
 pathPostings = "../output/postings.txt"
 pathDict = "../output/dictionary.txt"
 
-test(pathDict, pathPostings)
+test()
 
 
 
